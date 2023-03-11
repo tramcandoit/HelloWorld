f871664d7a936ce7f0c46e24dd3ad16e5fcb1539
Phạm Cao Trí <22521525@gm.uit.edu.vn>
2023-03-11 10:05:18 +0700

    bổ sung hướng dẫn

--- FindArea.cpp
+++ FindArea.cpp
@@ -50,8 +50,10 @@ class Triangle : public Shape {
 int main() {
     
     float w, h, l;
+    cout <<  "Nhap ba canh cua 1 tam giac: ";
     cin >> w >> h >> l;
     Triangle A(w,h,l);
+    cout << "Dien tich tam giac la: (tra ve 0 neu khong phai la tam giac hop le)" << ' ';
     cout << A.Area() <<'\n';
     return 0;
 }
